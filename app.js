6dd1ee95ab5976ea6bbdc6e0374d24abd7239dce
jiyeouny <dhkdhk3737@gmail.com>
2020-06-23 02:49:55 +0900

    Day05 / #2.8 ~ 2.11

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+const app = express();
+
+// const handleHome = (req, res) => res.send("Hello from home");
+
+// const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+// app.get("/", handleHome);
+
+// app.get("/profile", handleProfile);
+
+// app.use("/", globalRouter);
+app.use(routes.home, globalRouter);
+// app.use("/user", userRouter);
+app.use(routes.users, userRouter);
+// app.use("/video", videoRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
